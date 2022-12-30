3888fdf9436f842d0b3b5e3966675512a1a2f8f9
ZeKap <kapismath@gmail.com>
2022-12-30 22:52:57 +0100

    copying one file to another place works

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <fstream>
+
+
+std::string fileFromPath = "/home/kap/test/copy-2/from/file.txt";
+std::string fileToPath = "/home/kap/test/copy-2/to/file.txt";
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::ifstream input(fileFromPath);
+    std::ofstream output(fileToPath);
+
+    // if there is a problem opening one of the files
+    if(!input.is_open() || !output.is_open()) {
+        std::cout << "Error" << std::endl;
+        return 1;
+    }
+
+    output << input.rdbuf();
+
+    input.close();
+    output.close();
+
     return 0;
 }
