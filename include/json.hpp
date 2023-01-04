dcaf564cb45ce01cfadff0872a9191f46d5cba45
ZeKap <kapismath@gmail.com>
2023-01-04 01:41:48 +0100

    added json parser lib (nlohmann-json) and config file link

---
+++ include/json.hpp
@@ -0,0 +1,1 @@
+/Users/kap/Projets/libs/nlohmann-json/json.hpp
--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "include/Entry.h"
+#include "include/json.hpp"
 
+std::string folderFromPath = "/Users/kap/trucs-tests/AllSyncer/from/";
+std::string folderToPath = "/Users/kap/trucs-tests/AllSyncer/to";
 
-std::string folderFromPath = "/home/kap/test/copy-2/from/";
-std::string folderToPath = "/home/kap/test/copy-2/to/";
+std::string configFile = "/Users/kap/trucs-tests/AllSyncer/config.json";
 
 int main() {
     AllSyncer::Entry input(folderFromPath);
